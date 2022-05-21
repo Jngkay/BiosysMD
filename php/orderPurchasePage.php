5023f522f78d9b26e21a4884f09da02667363829
Jookie262 <barrientos.134038@iloilo.sti.edu.ph>
2022-05-21 23:43:04 +0800

    Create Modal for Add Purchase Order

--- php/orderPurchasePage.php
+++ php/orderPurchasePage.php
@@ -2,7 +2,12 @@
 	<?php include('../include/header.php');?>
 	<?php include('../include/navbar.php');?>
 <body>
-	<div class="label clientOrder">List of Purchase Orders</div>
+	<div class="label clientOrder">
+		List of Purchase Orders
+		<button id="myBtn">Add</button>
+		<!-- The Modal -->
+		<?php include('../include/modal_PurchaseOrder.php');?>
+	</div>
 		<table id="purchaseOrder_list" class="display">
 			<thead>
 				<tr>
@@ -28,10 +33,42 @@
 			</tbody>
 		</table>
 </body>
-			<script>
+		<script>
 			$(document).ready( function () {
-    $('#purchaseOrder_list').DataTable();
-} );
+				$('#purchaseOrder_list').DataTable();
+
+
+				$("#myBtn").click(function(){
+					$("#myModal").show();
+				});
+
+				$(".next").click(function(e){
+					e.preventDefault();
+					$("#myModal").hide();
+					$("#addPurchaseOrder").show();
+					
+					for(var x = 1; x <= $("#numorder").val(); x++){
+						$("#items-add").append(`
+							<h4 id="Title">Item ${x}: </h4>
+							<div class="form-group">
+								<input type="text" class="form-control" name="unitprice" placeholder="Product">
+							</div>
+
+							<div class="form-group">
+								<input type="text" class="form-control" name="unitprice" placeholder="Quantity">
+							</div>
+						`);
+					};
+				});
+
+				$(".close").click(function(){
+					$("#myModal").hide();
+				});
+
+				$(".close").click(function(){
+					$("#addPurchaseOrder").hide();
+				});
+
+			} );
 		</script>
-	
 </html>
