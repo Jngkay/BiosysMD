f5c268dfa5d145a4ce2e83895e8a75029463e53a
Jngkay <angelicagrace.simbran@wvsu.edu.ph>
2022-05-21 21:48:36 +0800

    Create Client Order View Table

--- php/orderClientPage.php
+++ php/orderClientPage.php
@@ -1,6 +1,7 @@
 <html>
 	<?php include('../include/header.php');?>
 	<?php include('../include/navbar.php');?>
+	<?php include("../database/db.php");?>
 <body>
 	<div class="label clientOrder">List of Client Orders</div>
 		<table id="clientOrder_list" class="display">
@@ -8,60 +9,35 @@
 				<tr>
 					<th>Client Order No.</th>
 					<th>Client Name</th>
-					<th>Order List</th>
+					<th>Item Order</th>
 					<th>Status</th>
-					<th>Total Amount</th>	
-					<th>Invoice No.</th>
 					<th>Action</th>
 				</tr>
 			</thead>
 			<tbody>
-				<tr>
-					<td>11000</td>
-					<td>Josmef Enterprises</td>	
-					<td>Facemask</td>	
-					<td>Complete</td>	
-					<td>1,200.00</td>	
-					<td>1614</td>	
-					<td><button class="action update">Update</button><button class="action delete">Delete</button></td>	
-				</tr>
-				<tr>
-					<td>11001</td>
-					<td>Medicus Philippines Inc.</td>	
-					<td>Urine Strips 10SG</td>	
-					<td>Partial</td>	
-					<td>15,000.00</td>	
-					<td>1615</td>	
-					<td><button class="action update">Update</button><button class="action delete">Delete</button></td>	
-				</tr>
-				<tr>
-					<td>11002</td>
-					<td>Medicus Philippines Inc.</td>	
-					<td>Urine Strips 4SG</td>	
-					<td>Partial</td>	
-					<td>640.00</td>	
-					<td>1616</td>	
-					<td><button class="action update">Update</button><button class="action delete">Delete</button></td>	
-				</tr>
-				<tr>
-					<td>11003</td>
-					<td>Qualimed Hospital</td>	
-					<td>Urine Strips 4SG</td>	
-					<td>Complete</td>	
-					<td>6,400.00</td>	
-					<td>1617</td>	
-					<td><button class="action update">Update</button><button class="action delete">Delete</button></td>	
-				</tr>
-				<tr>
-					<td>11004</td>
-					<td>HealthSense Laboratory</td>	
-					<td>Solution Pack Calcium</td>	
-					<td>Complete</td>	
-					<td>9,800.00</td>	
-					<td>1618</td>	
-					<td><button class="action update">Update</button><button class="action delete">Delete</button></td>	
-				</tr>
-			</tbody>
+			<tbody>
+					<?php
+						$sql = "SELECT * FROM clientorder"; //get all products
+						$result = $conn->query($sql);
+					?>
+
+					<?php if ($result->num_rows > 0): ?>
+						<?php while($row = $result->fetch_assoc()): ?>
+							<tr>
+								<td><?php echo $row["ClientOrderNo"]; ?></td>
+								<td><?php echo $row["ClientName"]; ?></td>	
+								<td><?php echo $row["OrderItem"]; ?></td>	
+								<td><?php echo $row["Status"]; ?></td>	
+								<td><button class="action update">Update</button>
+								<a class="action delete">Delete</td>	
+							</tr>
+						<?php endwhile; ?>
+					<?php endif; ?>
+
+					<?php
+						$conn->close();
+					?>
+				</tbody>
 		</table>
 </body>
 			<script>
