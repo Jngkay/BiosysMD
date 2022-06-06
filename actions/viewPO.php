17c5a49ec8a533eb186f7d4926b9d9b14a48c8b9
Jookie262 <barrientos.134038@iloilo.sti.edu.ph>
2022-06-06 17:16:58 +0800

    Add space between each orders

--- actions/viewPO.php
+++ actions/viewPO.php
@@ -6,26 +6,26 @@ if(isset($_POST['POorder_id'])){
     $query = "SELECT Orders, OrderQty,OrderPrice FROM purchaseorder WHERE PurchaseorderNo ='".$_POST['POorder_id']. "'";
     $REsult = mysqli_query($conn,$query);
 
-    $output .='
-    <div class="table-responsive">
-        <table class="table table-bordered">';
     while($row = mysqli_fetch_array($REsult))
     {
         $output .= '
-        <tr>
-            <td width="30%"><label> Order Item</label></td>
-            <td width="70%"> ' .$row["Orders"]. '</td>
-        </tr>
-        <tr>
-            <td width="30%"><label> Item Qty</label></td>
-            <td width="70%"> ' .$row["OrderQty"]. '</td>
-        </tr>
-        <tr>
-            <td width="30%"><label> Item Price</label></td>
-            <td width="70%"> ' .$row["OrderPrice"]. '</td>
-        </tr>
+        <div class="table-responsive">
+            <table class="table table-bordered"
+            <tr>
+                <td width="30%"><label> Order Item</label></td>
+                <td width="70%"> ' .$row["Orders"]. '</td>
+            </tr>
+            <tr>
+                <td width="30%"><label> Item Qty</label></td>
+                <td width="70%"> ' .$row["OrderQty"]. '</td>
+            </tr>
+            <tr>
+                <td width="30%"><label> Item Price</label></td>
+                <td width="70%"> ' .$row["OrderPrice"]. '</td>
+            </tr>
+            </table>
+        </div>
         ';
     }
-    $output .= '</table></div>';
     echo $output;
 }
