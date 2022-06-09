cac4b6616958e710b5c5faa1724758afdaaa861f
Jookie262 <barrientos.134038@iloilo.sti.edu.ph>
2022-06-09 18:38:09 +0800

    Display the unit price of the product that is selected

--- include/modalscript_orderPurchase.php
+++ include/modalscript_orderPurchase.php
@@ -1,7 +1,12 @@
 <script>
 			$(document).ready( function () {
-				$('#purchaseOrder_list').DataTable();
+				var itemprice = {};
+				var formatter = new Intl.NumberFormat('en-US', {
+				  style: 'currency',
+				  currency: 'PHP',
+				});
 
+				$('#purchaseOrder_list').DataTable();
 
 				$("#myBtn").click(function(){
 					$("#myModal").show();
@@ -15,9 +20,10 @@
 					for(var x = 1; x <= $("#numorder").val(); x++){
 						$("#items-add").append(`
 							<h4 id="Title">Item ${x}: </h4>
-							<select class="form-select" name="productId[]">
+							<select class="form-select" name="productId[]" id="select-products${x}">
                                 <?php if ($res->num_rows > 0): ?>
-                                    <?php while($row = $res->fetch_assoc()): ?>				
+                                    <?php while($row = $res->fetch_assoc()): ?>	
+                                    	${itemprice[<?php echo $row["ProdId"] ?>] = "<?php echo $row["ProdUnitPrice"]?>"};			
                                         <option value="<?php echo $row["ProdId"]?>"> <?php echo $row["ProdDescription"]?> </option>
                                     <?php endwhile; ?>
                                 <?php endif; ?>
@@ -27,10 +33,21 @@
                                 <input type="text" class="form-control" name="qty[]" placeholder="Quantity">
 							</div>
                             <div class="form-group">
-                            <input type="text" class="form-control" name="unitPrice[]" placeholder="Unit Price">
+                            	<input type="text" class="form-control" id="price${x}" name="unitPrice[]" placeholder="Unit Price">
 							</div>
 						`);
+
+						var getProductId = $("#select-products1 :selected").val();
+						$("#price" + x).val(formatter.format(itemprice[getProductId]));
 					};
+
+					$('select').change(function () {
+						var getItemId = $(this).prop("id");
+						var getnumber = parseInt(getItemId.replace(/[^0-9.]/g, ""));
+						var getProductId = $("#" + getItemId + ' :selected').val();
+						$("#price" + getnumber).val(formatter.format(itemprice[getProductId]));
+					}); 
+
 				});
 
 				$(".close").click(function(){
