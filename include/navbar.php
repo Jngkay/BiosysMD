46461de8783e3ca870740a0fa8ab26f6333f0867
Jngkay <angelicagrace.simbran@wvsu.edu.ph>
2022-06-09 18:40:07 +0800

    Add icons to navbar

--- include/navbar.php
+++ include/navbar.php
@@ -6,20 +6,27 @@
 			<h1 id="Biosys_name">Biosys Medical Trading<br><span id="tagline">Advanced Quality in Medical Technology</span></h1>
 		</div>
 		  <ul class="list-unstyled components">
-			  <li class="active">
-				  <a href="dashboard.php" id="label">Dashboard</a>
-			  </li> 
+			  <li>
+				  <a href="dashboard.php" id="label">
+				  	<i class="fi fi-rr-apps"></i>Dashboard</a>
+				</li>
 				<li>
-				  <a href="viewPage.php" id="label">View</a>
+					<a href="viewPage.php" id="label">
+					<i class="fi fi-rr-search"></i>View
+					</a>
+					
 			  </li>
 				<li>
-				  <a href="orderPage.php" id="label">Order</a>
+				  <a href="orderPage.php" id="label">
+				  <i class="fi fi-rr-shopping-cart"></i>Order</a>
 			  </li>
 				<li>
-				  <a href="settingPage.php" id="label">Settings</a>
+				  <a href="settingPage.php" id="label">
+				  <i class="fi fi-rr-settings-sliders"></i>Settings</a>
 			  </li>
 				<li>
-				  <a href="../index.php" id="label">Log out</a>
+				  <a id="label">
+				  <i class="fi fi-sr-sign-out"></i>Log out</a>
 			  </li>
 		  </ul>
 	</nav>
