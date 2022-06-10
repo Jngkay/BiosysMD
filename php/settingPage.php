bd803835863bd24c3e62dc4c970742f87331957e
Jngkay <angelicagrace.simbran@wvsu.edu.ph>
2022-06-10 20:41:12 +0800

    Show the username

--- php/settingPage.php
+++ php/settingPage.php
@@ -1,11 +1,29 @@
 <html>
 	<?php $page = 'settingPage'; include('../include/header.php');?>
+	<?php include('../database/db.php');?>
 	<?php include('../include/navbar.php');?>
 	<link href="../css/settingPage.css" rel="stylesheet" type="text/css" />
 	<body>
-		<div>
-			<div class="label">User Profile</div>
+	
+		<div class="container">
+			<div class="row">
+				<?php 
+				$sql="SELECT * FROM users";
+				$result=$conn->query($sql);?>
+				<?php if ($result->num_rows > 0): ?>
+					<?php while($row = $result->fetch_assoc()): ?>
+						<?php echo 'HI!' . $row["UserName"]; ?>
+					<?php endwhile; ?>
+				<?php endif; ?>
+			
+
+			</div>
+			<div class="row">
+				<div class="col-sm-6"></div>
+				<div class="col-sm-6"> </div>
+			</div>
 		</div>
+		<!--
 		<div class="form_update">
 			<form>
 				<label>Change Username</label>
@@ -16,7 +34,7 @@
 				<input type="password" placeholder="Enter New Password"><br>
 				<button>Save</button>
 			</form>
-		</div>
+		</div>!-->
 	</body>
 	
 </html>
