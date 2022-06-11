caf170452336e3b5b9504a1f9a50af318a1f18ae
Jingkay Simbran <101637701+Jngkay@users.noreply.github.com>
2022-06-11 18:59:06 +0800

    Change db variables for cloud hosting

--- database/db.php
+++ database/db.php
@@ -2,10 +2,10 @@
 
 // THIS IS USE EVERYTIME YOU CONNECT
 
-$servername = "localhost"; // LOCALHOST KAY XAMPP
-$username = "root"; // BY DEFAULT ITS ROOT
-$password = ""; // BY DEFAULT ITS EMPTY
-$dbname = "biosysdb"; // NAME OF YOUR DATABASE
+$servername = "remotemysql.com"; // LOCALHOST KAY XAMPP
+$username = "SFbfM40rI2"; // BY DEFAULT ITS ROOT
+$password = "8YlYXrYVeW"; // BY DEFAULT ITS EMPTY
+$dbname = "SFbfM40rI2"; // NAME OF YOUR DATABASE
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
