992acebdab0aff4fe71fb89d6a95ef9427df51b7
Jngkay <angelicagrace.simbran@wvsu.edu.ph>
2022-06-11 19:45:32 +0800

    Delete username input field

--- php/settingPage.php
+++ php/settingPage.php
@@ -8,13 +8,9 @@
 		</div>
 		<div class="form_update">
 			<form>
-				<label>Change Username</label>
-				<input type="text" placeholder="Enter Old Username">
-				<input type="text" placeholder="Enter New Username"><br><br>
 				<label>Change Password</label>
-				<input type="password" placeholder="Enter Old Password">
-				<input type="password" placeholder="Enter New Password"><br>
-				<button>Save</button>
+				<input type="password" placeholder="Password">
+				<button>Update</button>
 			</form>
 		</div>
 	</body>
